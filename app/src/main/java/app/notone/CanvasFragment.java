ef01ef9e2c5f7e0e41411edf36865751b6ab717b
default-student <l.h.6626@protonmail.com>
2022-11-04 12:58:15 +0100

    finished basic layout of the main canvas view, without burger menu

---
+++ app/src/main/java/app/notone/CanvasFragment.java
@@ -0,0 +1,64 @@
+package app.notone;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.HashMap;
+
+import androidx.fragment.app.Fragment;
+
+public class CanvasFragment extends Fragment {
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    String TAG = "NotOneCanvasFragment";
+    HashMap<String, Integer> penColors = new HashMap<>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        View view = inflater.inflate(R.layout.fragment_canvas, parent, false);
+
+        penColors.put("RED", Color.RED);
+        penColors.put("GREEN", Color.GREEN);
+        penColors.put("BLUE", Color.BLUE);
+
+        CanvasView canvasView = view.findViewById(R.id.canvasView);
+        Spinner dropdownPenColor = view.findViewById(R.id.spinner_pen);
+        ArrayAdapter<CharSequence> dropdownItems = ArrayAdapter.createFromResource(
+                getActivity(), R.array.pen_colors, R.layout.pen_color_spinner_dropdown_field);
+
+        dropdownItems.setDropDownViewResource(R.layout.pen_color_spinner_dropdown_items);
+        dropdownPenColor.setAdapter(dropdownItems); // set to spinner
+
+        class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, "onItemSelected: " + adapterView.getItemAtPosition(i));
+                canvasView.setStrokeColor(penColors.get(adapterView.getItemAtPosition(i)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        }
+        dropdownPenColor.setOnItemSelectedListener(new ItemSelectedListener());
+        return view;
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
--- app/src/main/java/app/notone/MainActivity.java
+++ app/src/main/java/app/notone/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -15,39 +14,13 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "NotOneMainActivity";
-    HashMap<String, Integer> penColors = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canvas);
+        setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
-        penColors.put("RED", Color.RED);
-        penColors.put("GREEN", Color.GREEN);
-        penColors.put("BLUE", Color.BLUE);
 
-        CanvasView canvasView = findViewById(R.id.canvasView);
-        Spinner dropdownPenColor = findViewById(R.id.spinner_pen);
-        ArrayAdapter<CharSequence> dropdownItems = ArrayAdapter.createFromResource(
-                this, R.array.pen_colors, R.layout.pen_color_spinner_dropdown_field);
-
-        dropdownItems.setDropDownViewResource(R.layout.pen_color_spinner_dropdown_items);
-        dropdownPenColor.setAdapter(dropdownItems); // set to spinner
-
-        class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
-
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d(TAG, "onItemSelected: " + adapterView.getItemAtPosition(i));
-                canvasView.setStrokeColor(penColors.get(adapterView.getItemAtPosition(i)));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        }
-        dropdownPenColor.setOnItemSelectedListener(new ItemSelectedListener());
     }
 }
