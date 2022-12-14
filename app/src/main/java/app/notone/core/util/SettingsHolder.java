121ec9fc30940b18c1d77e0d3fdce100dff23737
generic-student <kai.titgens@stud.th-owl.de>
2022-12-14 22:09:17 +0100

    added SettingsHolder which reflects the state of all Settings in the SettingsFragment

---
+++ app/src/main/java/app/notone/core/util/SettingsHolder.java
@@ -0,0 +1,70 @@
+package app.notone.core.util;
+
+import android.content.SharedPreferences;
+
+public class SettingsHolder {
+    //visuals
+    private static boolean sDarkMode = false;
+    private static boolean sShowPdfBounds = false;
+    private static boolean sTextMarkerOpacityAdding = true;
+
+    //function
+    private static boolean sRestrictedPanning = true;
+    private static boolean sDrawerSwipeOpen = false;
+    private static boolean sAutoSaveCanvas = false;
+    private static int sAutoSaveCanvasIntervalSeconds = 0;
+    private static boolean sSynchronizeWithServer = false;
+    private static int sSynchronizeWithServerIntervalSeconds = 0;
+
+    public static void update(SharedPreferences prefs) {
+        //visuals
+        sDarkMode = prefs.getBoolean("darkmode", false);
+        sShowPdfBounds = prefs.getBoolean("pdfbounds", false);
+        sTextMarkerOpacityAdding = prefs.getBoolean("textmarker", true);
+
+        //function
+        sRestrictedPanning =  prefs.getBoolean("twofingerpanning", false);
+        sDrawerSwipeOpen =  prefs.getBoolean("drawerswipe", false);
+        sAutoSaveCanvasIntervalSeconds =  prefs.getInt("saveintervall", Integer.MAX_VALUE);
+        sAutoSaveCanvas =  prefs.getBoolean("autosave", false);
+        sSynchronizeWithServer =  prefs.getBoolean("sync", false);
+        sSynchronizeWithServerIntervalSeconds =  prefs.getInt("syncintervall", Integer.MAX_VALUE);
+
+    }
+
+    public static boolean isDarkMode() {
+        return sDarkMode;
+    }
+
+    public static boolean isShowPdfBounds() {
+        return sShowPdfBounds;
+    }
+
+    public static boolean isTextMarkerOpacityAdding() {
+        return sTextMarkerOpacityAdding;
+    }
+
+    public static boolean isRestrictedPanning() {
+        return sRestrictedPanning;
+    }
+
+    public static boolean isDrawerSwipeOpen() {
+        return sDrawerSwipeOpen;
+    }
+
+    public static boolean isAutoSaveCanvas() {
+        return sAutoSaveCanvas;
+    }
+
+    public static int getAutoSaveCanvasIntervalSeconds() {
+        return sAutoSaveCanvasIntervalSeconds;
+    }
+
+    public static boolean isSynchronizeWithServer() {
+        return sSynchronizeWithServer;
+    }
+
+    public static int getSynchronizeWithServerIntervalSeconds() {
+        return sSynchronizeWithServerIntervalSeconds;
+    }
+}
