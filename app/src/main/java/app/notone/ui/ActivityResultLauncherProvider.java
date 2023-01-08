a84cb4a0a104de94282684da0d299f056e8d8c0a
default-student <l.h.6626@protonmail.com>
2023-01-08 15:36:45 +0100

    doc

--- app/src/main/java/app/notone/ui/ActivityResultLauncherProvider.java
+++ app/src/main/java/app/notone/ui/ActivityResultLauncherProvider.java
@@ -12,14 +12,20 @@ import app.notone.io.FileManager;
 import app.notone.ui.fragments.CanvasFragment;
 
 /**
- * Provides the ActivityResultLaunchers for the MainActivity and Ui
- * they enable the save and open functionality in the navDrawer
+ * Provides the ActivityResultLaunchers for the MainActivity and UI
+ * They enable the save and open functionality in the navDrawer
  * @author Luca Hackel
  * @since 202212XX
  */
 
 public class ActivityResultLauncherProvider {
 
+    /**
+     * Provides an ActivityResultLauncher that opens a CreateDocument Activity to chose a uri.
+     * This uri is used as save location for the pdf export.
+     * @param mainActivity app context
+     * @return ActivityResultLauncher
+     */
     @NonNull
     public static ActivityResultLauncher<String> getExportPdfActivityResultLauncher(MainActivity mainActivity) {
         return mainActivity.registerForActivityResult(new ActivityResultContracts.CreateDocument("application/pdf"), uri -> {
@@ -31,6 +37,12 @@ public class ActivityResultLauncherProvider {
         });
     }
 
+    /**
+     * Provides an ActivityResultLauncher that opens a CreateDocument Activity to chose a uri.
+     * The new canvas file is created at this location.
+     * @param mainActivity app context
+     * @return
+     */
     @NonNull
     public static ActivityResultLauncher<String> getNewCanvasFileActivityResultLauncher(MainActivity mainActivity) {
         return mainActivity.registerForActivityResult(new ActivityResultContracts.CreateDocument("application/json"), uri -> {
@@ -42,6 +54,12 @@ public class ActivityResultLauncherProvider {
         });
     }
 
+    /**
+     * Provides an ActivityResultLauncher that opens a OpenDocument Activity to chose the uri.
+     * The canvas file is opened from this uri.
+     * @param mainActivity app context
+     * @return
+     */
     @NonNull
     public static ActivityResultLauncher<String[]> getOpenCanvasFileActivityResultLauncher(MainActivity mainActivity) {
         return mainActivity.registerForActivityResult(new ActivityResultContracts.OpenDocument(), uri -> {
@@ -53,6 +71,12 @@ public class ActivityResultLauncherProvider {
         });
     }
 
+    /**
+     * Provides an ActivityResultLauncher that opens a CreateDocument Activity to chose the uri.
+     * The currently opened canvas is saved to this uri.
+     * @param mainActivity app context
+     * @return
+     */
     @NonNull
     public static ActivityResultLauncher<String> getSaveAsCanvasFileActivityResultLauncher(MainActivity mainActivity) {
         return mainActivity.registerForActivityResult(new ActivityResultContracts.CreateDocument("application/json"), uri -> {
@@ -64,6 +88,12 @@ public class ActivityResultLauncherProvider {
         });
     }
 
+    /**
+     * Provides an ActivityResultLauncher that opens a GetContent Activity to chose the uri.
+     * A PDF is imported from this uri into the canvas.
+     * @param canvasFragment
+     * @return
+     */
     @NonNull
     public static ActivityResultLauncher<String> getImportPdfActivityResultLauncher(Fragment canvasFragment) {
         return canvasFragment.registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
